ebcfb94e6b7eab192a7e4f64d4f3a5a8733353fd
Nabil Ridhwan <nabridhwan@gmail.com>
2022-08-21 16:46:39 +0800

    feat: :lipstick: Add base styles

---
+++ components/Section.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import { ReactNode } from 'react';
+
+const Section: NextPage<{ children: ReactNode }> = ({ children }) => {
+	return <div className="my-5">{children}</div>;
+};
+
+export default Section;
