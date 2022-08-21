d949af0e25e7d764d12be70a2d63a4d8986e8898
Nabil Ridhwan <nabridhwan@gmail.com>
2022-08-22 02:45:56 +0800

    feat: :sparkles: Add progress bar for pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,22 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+// Progress bar: https://stackoverflow.com/a/60755417
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+NProgress.configure({
+	minimum: 0.3,
+	easing: 'ease',
+	speed: 500,
+	showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
