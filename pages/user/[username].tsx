189e8163d3ad228c8114b9b65d754f59b30c9574
Nabil Ridhwan <nabridhwan@gmail.com>
2022-08-22 12:27:20 +0800

    feat: :test_tube: Testing: Top songs are part of SSR for user page

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -1,15 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
+import Head from 'next/head';
 import Link from 'next/link';
 import Container from '../../components/Container';
 import MusicPlayer from '../../components/MusicPlayer';
 import Section from '../../components/Section';
 import SongCard from '../../components/SongCard';
 import getCurrentSong from '../../fe_controller/song/getCurrentSong';
-import getTopSongs from '../../fe_controller/song/getTopSongs';
 import axiosInstance from '../../util/axiosInstance';
 
 type UsersProps = {
 	user: any;
+	top: any;
 };
 
 export async function getServerSideProps(context: any) {
@@ -17,15 +18,19 @@ export async function getServerSideProps(context: any) {
 
 	try {
 		const user = await axiosInstance.get(`/api/user/${username}`);
-		const rtn = user.data.data;
+		const top = await axiosInstance.get(`/api/song/top/${username}`);
 
-		if (!rtn.spotify_users) {
+		const userRtn = user.data.data;
+		const topRtn = top.data.data;
+
+		if (!userRtn.spotify_users) {
 			throw new Error('No spotify user found');
 		}
 
 		return {
 			props: {
-				user: rtn,
+				user: userRtn,
+				top: topRtn,
 			},
 		};
 	} catch (error) {
@@ -39,8 +44,9 @@ export async function getServerSideProps(context: any) {
 	}
 }
 
-const UserPage = ({ user }: UsersProps) => {
+const UserPage = ({ user, top }: UsersProps) => {
 	console.log(user);
+	console.log(top);
 
 	// const {
 	// 	data: recentSongsData,
@@ -64,92 +70,105 @@ const UserPage = ({ user }: UsersProps) => {
 		async () => await getCurrentSong(user.username)
 	);
 
-	const {
-		data: topSongsData,
-		isLoading: isTopSongsLoading,
-		isError: isTopSongsError,
-		isSuccess: isTopSongsSuccess,
-		status: topSongsStatus,
-	} = useQuery(
-		['topSongs', user.username],
-		async () => await getTopSongs(user.username),
-		{ onSuccess: (d) => console.log(d) }
-	);
+	// const {
+	// 	data: topSongsData,
+	// 	isLoading: isTopSongsLoading,
+	// 	isError: isTopSongsError,
+	// 	isSuccess: isTopSongsSuccess,
+	// 	status: topSongsStatus,
+	// } = useQuery(
+	// 	['topSongs', user.username],
+	// 	async () => await getTopSongs(user.username),
+	// 	{ onSuccess: (d) => console.log(d) }
+	// );
 
 	return (
-		<Container>
-			<Section>
-				{/* Profile Picture */}
-				{user.spotify_users && user.spotify_users.profile_pic_url && (
-					<img
-						className="rounded-full w-40"
-						src={user.spotify_users.profile_pic_url}
-						alt={user.spotify_users.name}
-					/>
-				)}
+		<>
+			<Head>
+				<title>{decodeURI(user.name)}&apos;s Profile</title>
 
-				{/* Name */}
-				<h2>{decodeURI(user.name)}</h2>
-
-				{/* Username */}
-				<p>@{user.username}</p>
-
-				{/* Spotify link */}
-				{user.spotify_users && (
-					<>
-						<Link
-							href={`https://open.spotify.com/user/${user.spotify_users.spotify_userid}`}
-						>
-							<a>Spotify</a>
-						</Link>
-					</>
-				)}
-
-				{currentSongStatus === 'success' &&
-					Object.keys(currentSongData).length > 0 && (
-						<>
-							<p>Currently Listening to</p>
-							<MusicPlayer
-								name={currentSongData.item.name}
-								artists={currentSongData.item.artists
-									.map((a: any) => a.name)
-									.join(', ')}
-								imageUrl={
-									currentSongData.item.album.images[2].url
-								}
-								spotifyLink={
-									currentSongData.item.external_urls?.spotify
-								}
+				<meta
+					name="description"
+					content={`Check out my profile! My top songs are ${top
+						.slice(0, 3)
+						.map((song: any) => song.name)
+						.join(', ')}`}
+				></meta>
+			</Head>
+			<Container>
+				<Section>
+					{/* Profile Picture */}
+					{user.spotify_users &&
+						user.spotify_users.profile_pic_url && (
+							<img
+								className="rounded-full w-40"
+								src={user.spotify_users.profile_pic_url}
+								alt={user.spotify_users.name}
 							/>
+						)}
+
+					{/* Name */}
+					<h2>{decodeURI(user.name)}</h2>
+
+					{/* Username */}
+					<p>@{user.username}</p>
+
+					{/* Spotify link */}
+					{user.spotify_users && (
+						<>
+							<Link
+								href={`https://open.spotify.com/user/${user.spotify_users.spotify_userid}`}
+							>
+								<a>Spotify</a>
+							</Link>
 						</>
 					)}
 
-				{topSongsStatus === 'success' &&
+					{currentSongStatus === 'success' &&
+						Object.keys(currentSongData).length > 0 && (
+							<>
+								<p>Currently Listening to</p>
+								<MusicPlayer
+									name={currentSongData.item.name}
+									artists={currentSongData.item.artists
+										.map((a: any) => a.name)
+										.join(', ')}
+									imageUrl={
+										currentSongData.item.album.images[2].url
+									}
+									spotifyLink={
+										currentSongData.item.external_urls
+											?.spotify
+									}
+								/>
+							</>
+						)}
+
+					{/* {topSongsStatus === 'success' &&
 					topSongsData &&
-					topSongsData.length > 0 && (
-						<>
-							<h2>Top Songs</h2>
-							<div className="grid grid-cols-2 md:grid-cols-5 gap-5">
-								{topSongsData.map((currentSong: any) => (
-									<SongCard
-										key={currentSong.id}
-										name={currentSong.name}
-										artists={currentSong.artists
-											.map((a: any) => a.name)
-											.join(', ')}
-										imageUrl={
-											currentSong.album.images[0].url
-										}
-										spotifyLink={
-											currentSong.external_urls?.spotify
-										}
-									/>
-								))}
-							</div>
-						</>
-					)}
-			</Section>
-		</Container>
+					topSongsData.length > 0 && ( */}
+					<>
+						<h2>Top Songs</h2>
+						<div className="grid grid-cols-2 md:grid-cols-5 gap-5">
+							{top.map((currentSong: any) => (
+								<SongCard
+									key={currentSong.id}
+									name={currentSong.name}
+									artists={currentSong.artists
+										.map((a: any) => a.name)
+										.join(', ')}
+									imageUrl={currentSong.album_art}
+									spotifyLink={
+										currentSong.external_urls?.spotify
+									}
+								/>
+							))}
+						</div>
+					</>
+					{/* )} */}
+				</Section>
+			</Container>
+		</>
 	);
 };
 
