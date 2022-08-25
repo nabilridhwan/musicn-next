56baa43a96829d0250a1fe99717b7c1ab4493ce6
Nabil Ridhwan <nabridhwan@gmail.com>
2022-08-25 13:07:25 +0800

    feat: :sparkles: New users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,5 +1,8 @@
+import { motion } from 'framer-motion';
 import absoluteUrl from 'next-absolute-url';
 import Link from 'next/link';
+import { FaSpotify } from 'react-icons/fa';
+import { IoPerson } from 'react-icons/io5';
 import CenterStage from '../components/CenterStage';
 import Container from '../components/Container';
 import Section from '../components/Section';
@@ -26,22 +29,38 @@ const Users = ({ users }: UsersProps) => {
 		<CenterStage>
 			<Container>
 				<Section>
-					{users.map((user, index) => {
-						return (
-							<div
-								key={index}
-								className="border border-white/50 break-all my-10 p-5 rounded-xl"
-							>
-								<h2 className="text-2xl font-bold">
-									{decodeURI(user.name)}
-								</h2>
-								<p className="muted text-sm">
-									@{user.username}
-								</p>
+					<header className="my-10">
+						<h1>Musicn Users</h1>
+						<p className="muted">All Musicn users</p>
+					</header>
+					<div className="grid md:grid-cols-2 gap-5">
+						{users.map((user, index) => {
+							return (
+								<div
+									key={index}
+									className="border border-white/20 break-all p-5 rounded-xl"
+								>
+									<h2
+										className={`text-2xl font-bold ${
+											!user.spotify_users &&
+											'text-white/20'
+										}`}
+									>
+										{decodeURI(user.name)}
+									</h2>
+									<p
+										className={`text-sm ${
+											!user.spotify_users
+												? 'text-white/20'
+												: 'muted'
+										}`}
+									>
+										@{user.username}
+									</p>
 
-								{user.spotify_users && (
-									<div>
-										{/* <p>
+									{user.spotify_users && (
+										<div className="flex flex-wrap gap-2">
+											{/* <p>
 											Spotify name:{' '}
 											{user.spotify_users.name}
 										</p>
@@ -50,16 +69,50 @@ const Users = ({ users }: UsersProps) => {
 											{user.spotify_users.spotify_userid}
 										</p> */}
 
-										<Link href={`/user/${user.username}`}>
-											<a className="btn btn-full my-2">
-												Profile
-											</a>
-										</Link>
-									</div>
-								)}
-							</div>
-						);
-					})}
+											{/* Profile Button */}
+											<motion.div
+												className="w-fit"
+												whileHover={{
+													scale: 1.1,
+												}}
+												whileTap={{
+													scale: 0.9,
+												}}
+											>
+												<Link
+													href={`/user/${user.username}`}
+												>
+													<a className="bg-white text-black font-bold shadow-[0px_0px_20px] shadow-white/20 rounded-lg px-4 py-2 w-fit mt-6 flex items-center gap-2">
+														<IoPerson size={16} />
+														Profile
+													</a>
+												</Link>
+											</motion.div>
+
+											<motion.div
+												className="w-fit"
+												whileHover={{
+													scale: 1.1,
+												}}
+												whileTap={{
+													scale: 0.9,
+												}}
+											>
+												<Link
+													href={`https://open.spotify.com/user/${user.spotify_users.spotify_userid}`}
+												>
+													<a className="bg-spotify font-bold shadow-[0px_0px_20px] shadow-spotify/50 rounded-lg px-4 py-2 w-fit mt-6 flex items-center gap-2">
+														<FaSpotify size={16} />
+														Spotify
+													</a>
+												</Link>
+											</motion.div>
+										</div>
+									)}
+								</div>
+							);
+						})}
+					</div>
 				</Section>
 			</Container>
 		</CenterStage>
