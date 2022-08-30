32922ff39d8a1fd2d6fbd825ccc50488dd2a51a1
Nabil Ridhwan <nabridhwan@gmail.com>
2022-08-30 23:28:58 +0800

    feat: :sparkles: Add RedirectHandler

--- util/RedirectHandler.ts
+++ util/RedirectHandler.ts
@@ -1,15 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 namespace RedirectHandler {
+	const redirectKey = 'redirect';
+	export function hasRedirect(req: NextApiRequest) {
+		return !!req.query[redirectKey];
+	}
 	export async function handleRedirect(
 		req: NextApiRequest,
 		res: NextApiResponse
 	) {
 		// TODO: Make an allowlist of allowed domains
-		const redirectRoute = req.query.redirectTo;
+		const redirectRoute = req.query[redirectKey];
 
 		if (redirectRoute) {
-			res.redirect(redirectRoute[0]);
+			return res.redirect(redirectRoute[0]);
 		}
 	}
 }
+
+export default RedirectHandler;
