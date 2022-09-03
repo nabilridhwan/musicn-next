77c86dc94960a368bee859170601f8520d82337f
Nabil Ridhwan <nabridhwan@gmail.com>
2022-09-03 21:34:19 +0800

    feat: :sparkles: Overhauled music player

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import { ReactNode } from 'react';
 
 const Section: NextPage<{ children: ReactNode }> = ({ children }) => {
-	return <div className="my-10 ">{children}</div>;
+	return <div className="my-10 mb-32">{children}</div>;
 };
 
 export default Section;
