b7e9250e0a0a8784969620c1e46c4a232daf4466
Nabil Ridhwan <nabridhwan@gmail.com>
2024-02-19 23:33:35 +0800

    fix: attempt to fix login/signup

--- util/auth.ts
+++ util/auth.ts
@@ -1,12 +1,11 @@
 // src/auth.ts
 import {Lucia, TimeSpan} from 'lucia';
 import {PrismaAdapter} from '@lucia-auth/adapter-prisma';
-import {PrismaClient} from '@prisma/client';
 import {Spotify} from 'arctic';
 
-const client = new PrismaClient();
+const client = prisma;
 
-const adapter = new PrismaAdapter(client.session, client.user);
+const adapter = new PrismaAdapter(client!.session, client!.user);
 
 export const lucia = new Lucia(adapter, {
   sessionExpiresIn: new TimeSpan(1, 'd'),
